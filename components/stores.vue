bb51ce2e9e7335d458b857786597466811152f3c
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-04-24 22:12:22 +0200

    index page and stores component

---
+++ components/stores.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="fav-stores">
+    <h3>{{ $t("popular-stores") }}</h3>
+    <div class="stores">
+      <ul>
+        <li v-for="n in 12">
+          <NuxtLink to="/">
+            <div class="store">
+              <img
+                src="https://cdn.almowafir.com/files/w100_adv_waffarha.jpg"
+                alt=""
+              />
+            </div>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <NuxtLink to="/stores">
+      <button class="see-more">{{ $t("see-more") }}</button>
+    </NuxtLink>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,99 @@
 <template>
   <div :id="`app-${this.$i18n.locale}`">
-    <navbar />
+    <div class="index-page">
+      <navbar />
+      <div class="container">
+        <div class="sides">
+          <div class="side">
+            <div class="carousel">
+              <swiper class="swiper">
+                <swiper-slide>
+                  <a href="#">
+                    <img src="https://cdn.almowafir.com/1/Shein-AR.jpeg" />
+                  </a>
+                </swiper-slide>
+              </swiper>
+            </div>
+            <div class="content">
+              <h2>
+                {{ $t("copouns-and-discounts") }} {{ new Date().getFullYear() }}
+              </h2>
+              <div class="cards">
+                <div class="card" v-for="n in 6">
+                  <div class="header">
+                    <NuxtLink to="/">
+                      <div class="brand">
+                        <img
+                          src="https://cdn.almowafir.com/files/w200_noon.png"
+                          alt=""
+                        />
+                      </div>
+                    </NuxtLink>
+                    <button
+                      class="share"
+                      @click="shareViaWebShare('test title', 'test content')"
+                    >
+                      <img src="/icons/icons8-share.svg" />
+                    </button>
+                  </div>
+                  <div class="content">
+                    <NuxtLink to="/">
+                      <h3>خصم حتى 20% على الموبايلات + 10% إضافي</h3>
+                    </NuxtLink>
+                  </div>
+                  <div class="footer">
+                    <NuxtLink to="/">
+                      <div class="copoun">
+                        <span> DG2 </span>
+                        <button>{{ $t("copy") }}</button>
+                      </div>
+                    </NuxtLink>
+                    <a href="/" target="_blank">
+                      <button class="offer" style="display: none">
+                        {{ $t("get-the-offer") }}
+                      </button>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="side">
+            <stores />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { Swiper, SwiperSlide, directive } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
 export default {
-  name: "IndexPage",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  directives: {
+    swiper: directive,
+  },
+  date() {
+    return {};
+  },
+  computed: {
+    webShareApiSupported() {
+      return navigator.share;
+    },
+  },
+  methods: {
+    shareViaWebShare(title, url) {
+      navigator.share({
+        title: title,
+        text: title,
+        url: url,
+      });
+    },
+  },
 };
 </script>
